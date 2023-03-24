cd8a85df0b279589085f2b67dbdb578c270b8929
Abdullah Jirjees <abdullah.jirjees@cgq.qc.ca>
2023-03-24 17:18:21 -0600

    Build Your Project

--- YoloSafety/YoloSafety.py
+++ YoloSafety/YoloSafety.py
@@ -3,11 +3,11 @@ import cv2
 import cvzone
 import math
 import time
-
+import pandas as ps
 # cap = cv2.VideoCapture(1)  # For Webcam
 # cap.set(3, 1280)
 # cap.set(4, 720)
-cap = cv2.VideoCapture("../Videos/ppe-2.mp4")  # For Video
+cap = cv2.VideoCapture("../Videos/ppe2.mp4")  # For Video
 
 
 model = YOLO("../Yolo-Weights/PPE.pt")
@@ -16,6 +16,9 @@ classNames = ['3', '4', '5', 'Helmet', 'boots', 'vest', 'head', 'helmet', 'no he
 
 prev_frame_time = 0
 new_frame_time = 0
+
+df = ps.DataFrame(columns=['Class', 'Confidence', 'X1', 'Y1', 'X2', 'Y2'])
+
 
 while True:
     new_frame_time = time.time()
@@ -36,6 +39,8 @@ while True:
             cls = int(box.cls[0])
 
             cvzone.putTextRect(img, f'{classNames[cls]} {conf}', (max(0, x1), max(35, y1)), scale=1, thickness=1)
+            df = df.append({'Class': classNames[cls], 'Confidence': conf, 'X1': x1, 'Y1': y1, 'X2': x2, 'Y2': y2}, ignore_index=True)
+
 
     fps = 1 / (new_frame_time - prev_frame_time)
     prev_frame_time = new_frame_time
@@ -43,3 +48,5 @@ while True:
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
+
+    df.to_excel('detections2.xlsx', index=False)
